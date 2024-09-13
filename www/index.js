2195aa2fe6ace6b24cc4308cd2593c496454a48e
JiSeong Lee <cocoquiet@knu.ac.kr>
2024-09-13 16:17:41 +0900

    Feat: Time profiling

--- www/index.js
+++ www/index.js
@@ -25,8 +25,9 @@ const resetButton = document.getElementById("reset");
 let animationId = null;
 
 const renderloop = () => {
-    universe.tick();
+    fps.render();
 
+    universe.tick();
     drawGrid();
     drawCells();
     
@@ -68,6 +69,44 @@ resetButton.addEventListener("click", event => {
     drawCells();
 });
 
+const fps = new class {
+    constructor() {
+        this.fps = document.getElementById("fps");
+        this.frames = [];
+        this.lastFrameTimeStamp = performance.now();
+    }
+
+    render() {
+        const now = performance.now();
+        const delta = now - this.lastFrameTimeStamp;
+        this.lastFrameTimeStamp = now;
+        const fps = 1 / delta * 1000;
+
+        this.frames.push(fps);
+        if (this.frames.length > 100) {
+            this.frames.shift();
+        }
+
+        let min = Infinity;
+        let max = -Infinity;
+        let sum = 0;
+        for (let i = 0; i < this.frames.length; i++) {
+            sum += this.frames[i];
+            min = Math.min(min, this.frames[i]);
+            max = Math.max(max, this.frames[i]);
+        }
+        let mean = sum / this.frames.length;
+
+        this.fps.textContent = `
+Frames per Second:
+latest = ${Math.round(fps)}
+avg of last 100 = ${Math.round(mean)}
+min of last 100 = ${Math.round(min)}
+max of last 100 = ${Math.round(max)}
+`.trim();
+    }
+}
+
 const drawGrid = () => {
     ctx.beginPath();
     ctx.strokeStyle = GRID_COLOR;
@@ -103,13 +142,30 @@ const drawCells = () => {
   
     ctx.beginPath();
   
+    ctx.fillStyle = ALIVE_COLOR;
     for (let row = 0; row < height; row++) {
         for (let col = 0; col < width; col++) {
             const idx = getIndex(row, col);
+            if (!bitIsSet(idx, cells)) {
+                continue;
+            }
   
-            ctx.fillStyle = bitIsSet(idx, cells)
-                ? ALIVE_COLOR
-                : DEAD_COLOR;
+            ctx.fillRect(
+                col * (CELL_SIZE + 1) + 1,
+                row * (CELL_SIZE + 1) + 1,
+                CELL_SIZE,
+                CELL_SIZE
+            );
+        }
+    }
+
+    ctx.fillStyle = DEAD_COLOR;
+    for (let row = 0; row < height; row++) {
+        for (let col = 0; col < width; col++) {
+            const idx = getIndex(row, col);
+            if (bitIsSet(idx, cells)) {
+                continue;
+            }
   
             ctx.fillRect(
                 col * (CELL_SIZE + 1) + 1,
